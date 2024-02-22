b0dfc26710f08d2a9c504d8d3be4d6598f472339
unknown <atikrahad1@gmail.com>
2024-02-22 06:55:10 +0600

    create get api for filter top price car

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const port = 5000;
 
 require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.1lk0tsy.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,15 +25,28 @@ async function run() {
     await client.connect();
     const CarsCollection = client.db("carsDB").collection("cars");
 
-    app.get("/resentcars", async(req, res)=>{
-      const  resentcars = await CarsCollection.find().sort({_id: -1}).limit(10).toArray()
-      res.send(resentcars)
-    })
+    app.get("/resentcars", async (req, res) => {
+      const resentcars = await CarsCollection.find()
+        .sort({ _id: -1 })
+        .limit(10)
+        .toArray();
+      res.send(resentcars);
+    });
+
+    app.get("/greateprice", async (req, res) => {
+      
+      const gretePrice = await CarsCollection.aggregate([
+        { $sort: { price: -1 } }, 
+        { $limit: 10 } 
+      ]).toArray()
+      
+      res.send(gretePrice)
+    });
 
     app.post("/car", async (req, res) => {
       const carInfo = req.body;
-      const result = await CarsCollection.insertOne(carInfo)
-      res.send(result)
+      const result = await CarsCollection.insertOne(carInfo);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
